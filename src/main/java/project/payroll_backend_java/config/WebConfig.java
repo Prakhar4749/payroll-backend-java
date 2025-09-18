ca88c229493e7b1763092936780dd446ea0b02b5
Prakhar sakhare~ <prakharsakhare2226@gmail.com>
2025-09-19 00:01:58 +0530

    cors

--- src/main/java/project/payroll_backend_java/config/WebConfig.java
+++ src/main/java/project/payroll_backend_java/config/WebConfig.java
@@ -3,6 +3,7 @@ package project.payroll_backend_java.config;
 import project.payroll_backend_java.component.DatabaseHealthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(databaseHealthInterceptor)
                 .addPathPatterns("/**"); // Apply to all routes
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://payroll.project.prakhar.life")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
